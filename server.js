c0e937874ffc1555086fac948c04beae02f1fdee
Regev Azriel <regevaz@gmail.com>
2019-06-07 00:46:04 +0300

    a

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 //server.js
+require('dotenv').config()
+const cloudinary = require('cloudinary')
+const formData = require('express-form-data')
+const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config')
+
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
+})
+  
+app.use(cors({ 
+  origin: CLIENT_ORIGIN 
+})) 
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -11,8 +28,21 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
+app.get('/wake-up', (req, res) => res.send('👌'))
+
+app.post('/image-upload', (req, res) => {
+
+  const values = Object.values(req.files)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+    .catch((err) => res.status(400).json(err))
+})
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+app.listen(port, () => console.log('👍'));
 
