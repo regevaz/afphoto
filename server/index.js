33d1fca5d91570d625d91fe13bd23a3d4c9faede
Regev Azriel <regevaz@gmail.com>
2019-06-07 02:25:38 +0300

    url

--- server/index.js
+++ server/index.js
@@ -24,12 +24,15 @@ app.use(cors({
   origin: CLIENT_ORIGIN 
 }))
 
+app.use(formData.parse())
+
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 app.get('/wake-up', (req, res) => res.send('👌'))
 
 app.post('/image-upload', (req, res) => {
 
+  console.log(`files: ${req.files}`);
   const values = Object.values(req.files)
   const promises = values.map(image => cloudinary.uploader.upload(image.path))
   
