3a0ea3da8cfae869fd4ad031ad17975f8e30b177
Regev Azriel <regevaz@gmail.com>
2019-06-10 22:43:01 +0300

    url

--- react-ui/src/Buttons.js
+++ react-ui/src/Buttons.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
+import upload from './upload.png'
 
 export default props => 
   <div className='buttons fadein'>
     <div className='button'>
       <label htmlFor='multi'>
-        <FontAwesomeIcon icon={faCloudUploadAlt} color='#6d84b4' size='10x' />
+        <img src={upload} className='upload-image' />
       </label>
       <input type='file' id='multi' onChange={props.onChange} multiple />
     </div>
--- react-ui/src/Spinner.js
+++ react-ui/src/Spinner.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBowlingBall } from '@fortawesome/free-solid-svg-icons'
+import loading from './loading.gif'
 
 export default () => 
-  <div className='rolling'>
-    <div className='spinner fadein'>
-      <FontAwesomeIcon icon={faBowlingBall} size='5x' color='#1D3C4C' />
-    </div>
-  </div>
+  <img src={loading} className='loading-image' />
